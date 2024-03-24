7502c3e97eb2fb1b31f185275a29f69883b27128
mymiragez <cmirage.pke@hotmail.com>
2024-03-24 15:46:37 +0700

    Edit call api

--- lib/views/show_value_iot04.dart
+++ lib/views/show_value_iot04.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, camel_case_types
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, camel_case_types, unnecessary_import, depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
